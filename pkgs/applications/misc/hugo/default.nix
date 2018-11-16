f81831da638fb31ace4a3fb355697cefec4bbac3
Mariusz `shd` Gliwiński <shd@nawia.net>
2018-11-16 18:02:00 +0100

    Revert "hugo: 0.32.2 -> 0.36.1"
    This reverts commit b332924e6aac9e34168f43cf7db5181bcd01f0e5.

--- pkgs/applications/misc/hugo/default.nix
+++ pkgs/applications/misc/hugo/default.nix
@@ -2,23 +2,23 @@
 
 buildGoPackage rec {
   name = "hugo-${version}";
-  version = "0.36.1";
+  version = "0.32.2";
 
   goPackagePath = "github.com/gohugoio/hugo";
 
   src = fetchFromGitHub {
-    owner  = "gohugoio";
-    repo   = "hugo";
-    rev    = "v${version}";
-    sha256 = "179lzkd6f81rssgcwlngx2mhr9nvbj0rsh6yjbf1fsrpxfzr2q9z";
+    owner = "gohugoio";
+    repo = "hugo";
+    rev = "v${version}";
+    sha256 = "0k62sg9rvr4aqzh1r60m456cw8mj6kxjpri2nnj4c2dxmvll8qhr";
   };
 
   goDeps = ./deps.nix;
 
-  meta = with stdenv.lib; {
+  meta = {
     description = "A fast and modern static website engine.";
     homepage = https://gohugo.io;
-    license = licenses.asl20;
-    maintainers = with maintainers; [ schneefux ];
+    maintainers = with stdenv.lib.maintainers; [ schneefux ];
+    license = stdenv.lib.licenses.asl20;
   };
 }
