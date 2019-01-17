fd2bf45fbb6930ecf99a090cc04dc539bd532877
Mariusz `shd` Gliwiński <shd@nawia.net>
2019-01-17 17:35:12 +0100

    revert hugo to lwy version

--- pkgs/applications/misc/hugo/default.nix
+++ pkgs/applications/misc/hugo/default.nix
@@ -1,37 +1,24 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, fetchpatch }:
+{ stdenv, buildGoPackage, fetchFromGitHub }:
 
 buildGoPackage rec {
   name = "hugo-${version}";
-  version = "0.47.1";
+  version = "0.18.1";
 
-  goPackagePath = "github.com/gohugoio/hugo";
+  goPackagePath = "github.com/spf13/hugo";
 
   src = fetchFromGitHub {
-    owner  = "gohugoio";
-    repo   = "hugo";
-    rev    = "v${version}";
-    sha256 = "0n27vyg66jfx4lwswsmdlybly8c9gy5rk7yhy7wzs3rwzlqv1jzj";
+    owner = "spf13";
+    repo = "hugo";
+    rev = "v${version}";
+    sha256 = "1nmabcrq96b339in2yr2zwcd41nadr4bha3rlpyaxlzbyyhz2f81";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/gohugoio/hugo/commit/b137ad4dbd6d14d0a9af68c044aaee61f2c87fe5.diff";
-      sha256 = "0w1gpg11idqywqcpwzvx4xabn02kk8y4jmyz4h67mc3yh2dhq3ll";
-    })
-  ];
 
   goDeps = ./deps.nix;
 
-  buildFlags = "-tags extended";
-
-  postInstall = ''
-    rm $bin/bin/generate
-  '';
-
-  meta = with stdenv.lib; {
+  meta = {
     description = "A fast and modern static website engine.";
     homepage = https://gohugo.io;
-    license = licenses.asl20;
-    maintainers = with maintainers; [ schneefux ];
+    maintainers = with stdenv.lib.maintainers; [ schneefux ];
+    license = stdenv.lib.licenses.asl20;
   };
 }
