26874219f7ebb047ccbbc5ffa24f9c2543b735a7
Mariusz `shd` Gliwiński <shd@nawia.net>
2026-02-10 02:01:14 +0100

    [rme] 3.7 -> 3.8-da7152ec94031c76732e997de4624c8f1c010225

--- pkgs/games/opentibia/rme/default.nix
+++ pkgs/games/opentibia/rme/default.nix
@@ -1,48 +1,49 @@
-{ lib, stdenv, fetchFromGitHub, cmake, libarchive, boost186, freeglut, libGLU, wxGTK31, zlib }:
+{ stdenv, fetchFromGitHub, cmake, wrapGAppsHook3, libarchive, boost186, fmt, freeglut, libGLU, nlohmann_json, wxGTK31, zlib }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "rme";
-  version = "3.7";
+  version = "3.8-dev";
 
   src = fetchFromGitHub {
     owner = "hampusborgos";
     repo = "rme";
-    rev = "v${version}";
-    sha256 = "1wqgr7cg04xq3hdq0ikz6s2ir15aal1f9mrymq7kwh3c173v3zpd";
+    rev = "da7152ec94031c76732e997de4624c8f1c010225";
+    hash = "sha256-YyAhaRlRkkcP2nrkaW2HWMyPL8pWoqt2qAMittA7+3k=";
   };
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [ cmake wrapGAppsHook3 ];
 
   buildInputs = [
     libarchive
     boost186
+    fmt
     freeglut
     libGLU
+    nlohmann_json
     wxGTK31
     zlib
   ];
 
-  patchPhase = ''
-    sed -i 's/GLUT_LIBRARY}/GLUT_LIBRARIES}/g' CMakeLists.txt
-  '';
+  patches = [
+    ./rme.patch
+  ];
 
   cmakeFlags = [
     "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
   ];
 
   installPhase = ''
-    cd .. # as cmake ends up in build/ directory, get out of there
-    mkdir $out
+        cd .. # as cmake ends up in build/ directory, get out of there
+        mkdir $out
 
-    mkdir $out/bin
-    cp build/rme $out/bin/rme
+        mkdir $out/bin
+        cp build/rme $out/bin/rme
 
-    cp -r brushes/ data/ extensions/ icons/ $out/
+        cp -r brushes/ data/ extensions/ icons/ $out/
   '';
 
   meta = {
     description = "Map editor for top-down MMORPG set in a fantasy world";
     homepage = "http://github.com/hampusborgos/rme";
-    license = lib.licenses.unfree;
   };
 }
